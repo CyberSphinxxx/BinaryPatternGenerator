414cf5a2af1493c73da1ad0a58193c0908708c69
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-02-13 13:50:11 +0800

    Binary Pattern Generator

---
+++ BinaryPatternGenerator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows;
+    int i;
+    int j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("%d", (i + j) % 2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
